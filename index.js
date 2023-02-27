64102aa50f2cb34457ad83373ee8e38921aee535
unknown <dmlswebdev@gmail.com>
2023-02-27 22:07:12 +0200

    License section working as intended. License types with multiple options prompt for sub-options to be chosen

--- index.js
+++ index.js
@@ -66,6 +66,55 @@ inquirer
       message: 'Enter links to their primary web presence\n\nEnsure they are in the same order as above\nUse the following format "https://www.example1.com, https://www.example2.com, https://www.example3.com"\n\n',
       when: (answers) => answers.credits_attributions_q === true,
     },
+    {
+      type: 'list',
+      choices:['None','Apache 2.0 License','Boost Software License 1.0','BSD (Multiple Options)','Creative Commons (Multiple Options)','Eclipse Public License 1.0','GNU (Multiple Options)','The Organization for Ethical Source (Multiple Options)','IBM Public License Version 1.0','ISC License (ISC)','The MIT License','Mozilla Public License 2.0','Open Data Commons (Multiple Options)','Perl (Multiple Options)','SIL Open Font License 1.1','The Unlicense','The Do What the Fuck You Want to Public License','The zlib/libpng License'],
+      name: 'license',
+      message: 'Select the appropriate license type:',
+    },
+    {
+      type: 'list',
+      choices:['BSD 2-Clause License','BSD 3-Clause License'],
+      name: 'license_BSD',
+      message: 'Select the appropriate "BSD" license type:',
+      when: (answers) => answers.license === 'BSD (Multiple Options)',
+    },
+    {
+      type: 'list',
+      choices:['CC0','Attribution 4.0 International','Attribution-ShareAlike 4.0 International','Attribution-NonCommercial 4.0 International','Attribution-NoDerivates 4.0 International','Attribution-NonCommmercial-ShareAlike 4.0 International','Attribution-NonCommercial-NoDerivatives 4.0 International'],
+      name: 'license_creative',
+      message: 'Select the appropriate "Creative Commons" license type:',
+      when: (answers) => answers.license === 'Creative Commons (Multiple Options)',
+    },
+    {
+      type: 'list',
+      choices:['GNU GPL v2','GNU GPL v3','GNU AGPL v3','GNU LGPL v3','GNU FDL v1.3'],
+      name: 'license_GNU',
+      message: 'Select the appropriate "GNU" license type:',
+      when: (answers) => answers.license === 'GNU (Multiple Options)',
+    },
+    {
+      type: 'list',
+      choices:['The Hippocratic License 2.1','The Hippocratic License 3.0'],
+      name: 'license_ethical',
+      message: 'Select the appropriate "The Organization for Ethical Source" license type:',
+      when: (answers) => answers.license === 'The Organization for Ethical Source (Multiple Options)',
+    },
+    {
+      type: 'list',
+      choices:['Attribution License (BY)','Open Database License (ODbL)','Public Domain Dedication and License (PDDL)'],
+      name: 'license_open_data',
+      message: 'Select the appropriate "Open Data Commons" license type:',
+      when: (answers) => answers.license === 'Open Data Commons (Multiple Options)',
+    },
+    {
+      type: 'list',
+      choices:['The Perl License','The Artistic License 2.0'],
+      name: 'license_perl',
+      message: 'Select the appropriate "Perl" license type:',
+      when: (answers) => answers.license === 'Perl (Multiple Options)',
+    },
+    
   ])
   // inquirer then with all instructions to write readme file
   .then((answers) => {
@@ -78,6 +127,13 @@ inquirer
     const projectCollaboratorsQ = answers.credits_collaborators_q;
     const projectAttributionsQ = answers.credits_attributions_q;
     const projectCreditsQs = [projectCollaboratorsQ, projectAttributionsQ];
+    const projectLicense = answers.license;
+    const projectLicenseBSD = answers.license_BSD;
+    const projectLicenseCreative = answers.license_creative;
+    const projectLicenseGNU = answers.license_GNU;
+    const projectLicenseEthical = answers.license_ethical;
+    const projectLicenseOpenData = answers.license_open_data;
+    const projectLicensePerl = answers.license_perl;
 
     let projectCollaboratorsNames;
     let projectCollaboratorsGits;
@@ -171,9 +227,32 @@ ${projectUsage}\n\n`
           };
 
 // readme file content for license information
-// !!! NOT COMPLETE !!!
     readmeContent += `## License\n
-### LICENSE INFO GOES HERE\n\n`
+This project uses the following license:\n\n`
+
+    switch (projectLicense) {
+      case 'BSD (Multiple Options)':
+        readmeContent += `**${projectLicenseBSD}**\n\n`
+        break;
+      case 'Creative Commons (Multiple Options)':
+        readmeContent += `**${projectLicense}**\n\n`
+        break;
+      case 'GNU (Multiple Options)':
+        readmeContent += `**${projectLicenseGNU}**\n\n`
+        break;
+      case 'The Organization for Ethical Source (Multiple Options)':
+        readmeContent += `**${projectLicenseEthical}**\n\n`
+        break;
+      case 'Open Data Commons (Multiple Options)':
+        readmeContent += `**${projectLicenseOpenData}**\n\n`
+        break;
+      case 'Perl (Multiple Options)':
+        readmeContent += `**${projectLicensePerl}**\n\n`
+        break;
+      default:
+        readmeContent += `**${projectLicense}**\n\n`
+        break;
+    }
 
 // readme file content for badges
 // !!! NOT COMPLETE !!!
