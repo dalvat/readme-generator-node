eb96a6e7fcec20754f82cb1aad1d1986e98a1344
unknown <dmlswebdev@gmail.com>
2023-02-27 15:14:36 +0200

    file creation working with basic input

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
-init();
+// init();
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+  ])
+  .then((answers) => {
+    const projectTitle = answers.title;
+    const readmeFileName = `./generated_files/${answers.title}-README.md`
+
+    const readmeContent = `# ${answers.title}\n\n### More content here.`
+
+    fs.writeFile(readmeFileName, readmeContent, (error) => {
+      return error
+        ? console.error(error)
+        : console.log('File Created Successfully!')
+    });
+
+  })
